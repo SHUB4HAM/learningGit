efd7b5f4714b302b8bc5e5970851f6f67fd72aa5
SHUB4HAM <shubhamdhoke23@gmail.com>
2023-05-30 15:59:02 +0530

    feature_1 added

---
+++ NewFeature/Feature_1_Added.java
@@ -0,0 +1,6 @@
+package NewFeature;
+public class Feature_1_Added {
+    public static void main(String[] args) {
+        System.out.println("THe feature 1 has been added to the project");
+    }
+}
